e99cca9f50eabb850727c1012ff92b5ee30105f4
kornienko199004 <kornienko199004@gmail.com>
2018-11-28 23:49:08 +0300

    mobile first

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import truck from '../img/truck.svg';
 import man from '../img/man.svg';
 import money from '../img/money.svg';
 import phone from '../img/phone.svg';
+import photo from '../img/photo.svg';
+import lock from '../img/lock.svg';
+import goodMoney from '../img/goodMoney.svg';
+import creditCard from '../img/creditCard.svg';
 
 console.log('hello, dudde!');
 const fn = () => {
