e455490def7747566e6f4f325485bed9cc963f43
kornienko199004 <kornienko199004@gmail.com>
2019-01-24 21:40:15 +0300

    Add phpmailer

--- action/photoAction.php
+++ action/photoAction.php
@@ -1,12 +1,45 @@
 <?php
+// Файлы phpmailer
+require 'class.phpmailer.php';
+require 'class.smtp.php';
 
-$name = htmlspecialchars($_POST['photo']);
-$phone = (int)$_POST['phone'];
+// Переменные
+$name = htmlspecialchars($_POST['phone-name']);
+$number = htmlspecialchars($_POST['phone']);
 
-$to = 'metalldemontag2019@gmail.com';
-$subject = 'Оценка по фотоЫ';
-$message = "Пользователь " . $name . ", номер телефона " . $phone;
+// Настройки
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Host = 'smtp.yandex.ru'; 
+$mail->SMTPAuth = true; 
+$mail->Username = 'kornienko199004'; // Ваш логин в Яндексе. Именно логин, без @yandex.ru
+$mail->Password = 'gsmg2404'; // Ваш пароль
+$mail->SMTPSecure = 'ssl'; 
+$mail->Port = 465;
+$mail->setFrom("kornienko199004@yandex.ru"); // Ваш Email
+$mail->addAddress("metalldemontag2019@gmail.com"); // Email получателя
 
-mail($to, $subject, $message);
+// Прикрепление файлов
+ for ($ct = 0; $ct < count($_FILES['userfile']['tmp_name']); $ct++) {
+ $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['userfile']['name'][$ct]));
+ $filename = $_FILES['userfile']['name'][$ct];
+ if (move_uploaded_file($_FILES['userfile']['tmp_name'][$ct], $uploadfile)) {
+ $mail->addAttachment($uploadfile, $filename);
+ } else {
+ $msg .= "Failed to move file to " . $uploadfile;
+ }
+ } 
+ 
+// Письмо
+$mail->isHTML(true); 
+$mail->Subject = 'Оценка по фото'; // Заголовок письма
+$mail->Body = "Имя $name . Телефон $number"; // Текст письма
 
-exit('<meta http-equiv="refresh" content="0; url=../" />');
+// Результат
+if(!$mail->send()) {
+ echo 'Message could not be sent.';
+ echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+ echo 'ok';
+}
