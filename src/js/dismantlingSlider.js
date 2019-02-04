fd115a2fb6220a307c4fdd55d6002267262de8ba
kornienko199004 <kornienko199004@gmail.com>
2019-02-04 23:30:48 +0300

    Improve dismantling slider

--- src/js/dismantlingSlider.js
+++ src/js/dismantlingSlider.js
@@ -1,8 +1,11 @@
 export default () => {
   const slider = document.querySelector('.dismantling__slider');
+  const previewButton = document.querySelector('.dismantling__button--preview');
+  const nextwButton = document.querySelector('.dismantling__button--next');
+  const currentSliderClass = 'dismantling__slide--current';
+  const { children } = slider;
 
   function setCurrentSliderClass() {
-    const currentSliderClass = 'dismantling__slide--current';
     if (!this.classList.contains(currentSliderClass)) {
       const currentSlider = slider.querySelector(`.${currentSliderClass}`);
       currentSlider.classList.remove(currentSliderClass);
@@ -10,6 +13,20 @@ export default () => {
     }
   }
 
-  const { children } = slider;
+  function setNextSliderTheCurrent() {
+    const currentSlideIndex = Array.from(children).reduce((acc, item, index) => (item.classList
+      .contains(currentSliderClass)
+      ? index : acc), '');
+    const rest = (currentSlideIndex + 1) % children.length;
+    const newCurrentSlideIndex = rest === 0 ? 0 : currentSlideIndex + 1;
+    const currentSlide = document.querySelector(`.${currentSliderClass}`);
+    currentSlide.classList.remove(currentSliderClass);
+    children[newCurrentSlideIndex].classList.add(currentSliderClass);
+  }
+
+
   Array.from(children).forEach(child => child.addEventListener('click', setCurrentSliderClass));
+
+  previewButton.addEventListener('click', () => console.log('preview'));
+  nextwButton.addEventListener('click', setNextSliderTheCurrent);
 };
