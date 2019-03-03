1021775d5015b0b5ab59028a46223b7322778021
kornienko199004 <kornienko199004@gmail.com>
2019-03-03 23:05:38 +0300

    Add meta tags

--- webpack.config.js
+++ webpack.config.js
@@ -89,3 +89,113 @@ module.exports = (env, options) => {
     : 'eval-sourcemap';
   return conf;
 };
+
+
+/*
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+/*
+const conf = {
+  entry: {
+    test: './src/index.js',
+ //   translate: './src/js/translate.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    publicPath: 'dist/',
+  },
+  devServer: {
+    overlay: true,
+    compress: true,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /js/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        }],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist/*']),
+        ],
+        optimization: {
+          usedExports: true
+        }
+};
+*/
+/*
+const conf = {
+  entry: {
+    test: './src/index.js',
+   // translate: './src/js/translate.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /js/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        }],
+      },
+    ],
+  },
+  plugins: [
+  new CleanWebpackPlugin(['dist/*']),
+  ],
+  optimization: {
+    splitChunks: {
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: false,
+        commons: {
+          name: 'translate',
+          test: /[\\/]src[\\/]js/,
+          chunks: 'all',
+          enforce: true,
+          minChunks: 1,
+        },
+      },
+    },
+  },
+};
+
+module.exports = (event, options) => {
+  const production = options.mode === 'production';
+  conf.devtool = production
+    ? 'eval-sourcemap'
+    : 'eval-sourcemap';
+  return conf;
+};*/
